dee221315718203d8ea3b6dd99007274d0101382
Daniil Suvorov <severecloud@gmail.com>
2024-01-26 21:06:29 +0300

    refactor(env): create gitlab_get_last_tag

--- update-env.sh
+++ update-env.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-jq --version || (echo "Need setup jq. https://jqlang.github.io/jq/download/" && exit 1)
+jq --version > /dev/null || (echo "Need setup jq. https://jqlang.github.io/jq/download/" && exit 1)
 
+# Return last tag from gitlab project
+#
+# Usage: gitlab_get_last_tag <domain> <project_id>
+# Example: gitlab_get_last_tag gitlab.torproject.org 647
+gitlab_get_last_tag()
+{
+  echo $(curl -s https://$1/api/v4/projects/$2/repository/tags | jq -r ".[0].name")
+}
+
+# Rewrute .env file
 cat << EOF > .env
-ARTI_VERSION=$(curl -s https://gitlab.torproject.org/api/v4/projects/647/repository/tags | jq -r ".[0].name")
-OBFS4_VERSION=$(curl -s https://gitlab.com/api/v4/projects/10387781/repository/tags | jq -r ".[0].name")
-SNOWFLAKE_VERSION=$(curl -s https://gitlab.torproject.org/api/v4/projects/43/repository/tags | jq -r ".[0].name")
+ARTI_VERSION=$(gitlab_get_last_tag gitlab.torproject.org 647)
+OBFS4_VERSION=$(gitlab_get_last_tag gitlab.com 10387781)
+SNOWFLAKE_VERSION=$(gitlab_get_last_tag gitlab.torproject.org 43)
 EOF
- 
+
+cat .env
