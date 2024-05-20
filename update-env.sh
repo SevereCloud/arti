260901aaf2f259ffd3b9b61f836112aa4abed22a
Daniil Suvorov <severecloud@gmail.com>
2024-05-20 10:57:49 +0300

    fix: use startswith

--- update-env.sh
+++ update-env.sh
@@ -11,9 +11,19 @@ gitlab_get_last_tag()
   echo $(curl -s https://$1/api/v4/projects/$2/repository/tags | jq -r ".[0].name")
 }
 
+
+# Return last subtag from gitlab project
+#
+# Usage: gitlab_get_last_tag <domain> <project_id> <startwith>
+# Example: gitlab_get_last_tag gitlab.torproject.org 647 arti
+gitlab_get_last_subtag()
+{
+  echo $(curl -s https://$1/api/v4/projects/$2/repository/tags | jq -r "[.[].name|select(startswith(\"$3\"))][0]")
+}
+
 # Rewrute .env file
 cat << EOF > .env
-ARTI_VERSION=$(gitlab_get_last_tag gitlab.torproject.org 647)
+ARTI_VERSION=$(gitlab_get_last_subtag gitlab.torproject.org 647 arti)
 OBFS4_VERSION=$(gitlab_get_last_tag gitlab.com 10387781)
 SNOWFLAKE_VERSION=$(gitlab_get_last_tag gitlab.torproject.org 43)
 WEBTUNNEL_VERSION=399c24557a186fcb8ca079f6be38d9cba5fa8ec5
